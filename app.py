133d98c23ab9bdad09154ca573cce01b84de728f
Rupesh Yadav <ruppeshyadav7317@gmail.com>
2026-04-13 15:24:25 +0530

    Add normalization to image transformations
    Added normalization to the image transformation pipeline.

--- app.py
+++ app.py
@@ -14,7 +14,8 @@ model.eval()
 transform = transforms.Compose([
     transforms.Resize((224, 224)),
     transforms.Grayscale(num_output_channels=3),
-    transforms.ToTensor()
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
 ])
 
 # UI
